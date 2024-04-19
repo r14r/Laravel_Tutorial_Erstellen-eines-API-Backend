ead8f9bc152e749ff66a7ba4289e461aceb5bae3
Ralph Göstenmeier <r14r@via-internet.de>
2024-04-19 17:56:16 +0200

    -

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/get',            [PostController::class, 'index']);
-Route::post('/post',          [PostController::class, 'store']);
-Route::delete('/delete/{id}', [PostController::class, 'destroy']);
+Route::get('/post',         [PostController::class, 'index']);
+Route::post('/post',        [PostController::class, 'store']);
+Route::delete('/post/{id}', [PostController::class, 'destroy']);
